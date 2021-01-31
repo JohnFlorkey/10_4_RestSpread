7e428d9e12e58c093600fb0b20ee63abaffba173
John Florkey <john.florkey@gmail.com>
2021-01-31 18:01:00 -0500

    findMin working

---
+++ app.js
@@ -0,0 +1,15 @@
+function filterOutOdds() {
+    var nums = Array.prototype.slice.call(arguments);
+    return nums.filter(function(num) {
+      return num % 2 === 0
+    });
+  }
+  
+function myFilterOutOdds(...vals) {
+    return vals.filter((val) => val % 2 === 0);
+}
+
+function findMin(...nums) {
+    return nums.reduce((localMin, nextNum) => nextNum < localMin ? nextNum : localMin);
+}
+
