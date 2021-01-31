c5e98ac6033216d2993056a186147c9c634f8d21
John Florkey <john.florkey@gmail.com>
2021-01-31 18:03:51 -0500

    memrgeObjects working

--- app.js
+++ app.js
@@ -13,3 +13,6 @@ function findMin(...nums) {
     return nums.reduce((localMin, nextNum) => nextNum < localMin ? nextNum : localMin);
 }
 
+function mergeObjects(obj1, obj2) {
+    return {...obj1, ...obj2};
+}
